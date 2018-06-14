9d7273bd7e08e86fd786bfffc1ae717849722429
Ten <ten@chuwa.iobb.net>
2018-06-14 12:02:06 +0900

    Initial release.

---
+++ minimalSample/main.js
@@ -0,0 +1,11 @@
+
+
+var csInterface = new CSInterface();
+
+function callJSXScript(){
+        csInterface.evalScript("func1()");
+    }
+function callAction(){
+        csInterface.evalScript("func2()");
+    }
+
