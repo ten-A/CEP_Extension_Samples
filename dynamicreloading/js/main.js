0483eb7e679ffc751bd47da1e80cf359716e6218
Ten <ten@chuwa.iobb.net>
2018-07-31 07:59:17 +0900

    make IIFE

--- dynamicreloading/js/main.js
+++ dynamicreloading/js/main.js
@@ -1,14 +1,16 @@
-var CSLibrary = new CSInterface();
-themeSynchronizer.init(); 
-var mainJSX =  CSLibrary.getSystemPath(SystemPath.EXTENSION) + '/jsx/hostscript.jsx';
-var script = '$.evalFile("' + mainJSX + '");';
-CSLibrary.evalScript(script);
-
-$("#aply").click(function () {
-	var str = $("input#str").val();
-	CSLibrary.evalScript('testcall("' + str + '")',
-		function(re){
-			alert("alert from CEP side : " + re);
+(function(){
+	'use strict';
+	var CSLibrary = new CSInterface();
+	themeSynchronizer.init(); 
+	var mainJSX =  CSLibrary.getSystemPath(SystemPath.EXTENSION) + '/jsx/hostscript.jsx';
+	var script = '$.evalFile("' + mainJSX + '");';
+	CSLibrary.evalScript(script);
+	
+	$("#aply").click(function () {
+		var str = $("input#str").val();
+		CSLibrary.evalScript('testcall("' + str + '")',
+			function(re){
+				alert("alert from CEP side : " + re);
+			});
 		});
-	});
-
+})();
